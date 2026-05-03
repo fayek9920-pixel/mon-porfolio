69c84f3cb37cb477e0af9253756dc6e461496fae
fayek9920-pixel <fayek9920@gmail.com>
2026-05-03 20:26:30 +0000

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+with st.sidebar: 
+    
+     st.title ("KHADY FAYE")
+
+     st.info ("Technicienne supérieur en géomatique")
+
+     st.info("Mail:fayeK9920@gmail.com")
+
+     st.info ("ADRESSE= senegal ,Dakar ,pikine")
+
+st.header("Profil") 
+
+st.info ("je suis expert en géomatique ,motivée et sérieuse avec des compétences en cartographie et dessin de plan dans Arcmap je suis capable de travailler sur des projets de gestion de bases de données spatiales et de production des cartes")
+    
+st.header ("Formations")
+              
+st.write("* Baccalauriat")
+
+st.write("* Brevet de technicien supérieur en géomatique")
+
+st.header("compétences")
+
+st.write ("* Cartographie numérique")
+             
+st.write ("* Analyse spaciale")
+             
+st.write ("* Topographie et levés terrain")
+             
+st.write ("* Numérisation et géoréférencement")
+
+st.header ("Expériences")
+
+st.markdown("* Manipulation sur Arcgis ")
+
+st.markdown("* Numérisation de parcelle")
+
+st.markdown("* Réalisation de plan ou cartes")
+
+st.write("je vous remercie de l'attention portée à ma candidature")
+
+
+
